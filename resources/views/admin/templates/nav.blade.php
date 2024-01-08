b9dee13e01bdb25517ec476aa4ca06edab3411da
Pratik Bhujel <prateekbhujelpb@gmail.com>
2024-01-08 13:49:52 +0545

    Resource Controller created with Model Integration

--- resources/views/admin/templates/nav.blade.php
+++ resources/views/admin/templates/nav.blade.php
@@ -7,8 +7,8 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.staff.index') }}">
-                            <i class="fa-solid me-2"></i>Staffs
+                        <a class="nav-link" href="{{ route('admin.staffs.index') }}">
+                            <i class="fa-solid fa-users me-2"></i>Staffs
                         </a>
                     </li>
                 </ul>
